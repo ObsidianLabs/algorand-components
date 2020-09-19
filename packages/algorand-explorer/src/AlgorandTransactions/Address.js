51f2ef98070d196e710188463a509ba00dc21e15
Phil <qft.gtr@gmail.com>
2020-09-20 00:37:45 +0800

    use Link from react-router-dom

--- packages/algorand-explorer/src/AlgorandTransactions/Address.js
+++ packages/algorand-explorer/src/AlgorandTransactions/Address.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 export default function Address ({ addr }) {
   if (!addr) {
     return null
   }
   return (
-    <a href={`#/account/${addr}`} className='text-body small'>
+    <Link to={`/account/${addr}`} className='text-body small'>
       <code>{addr.substr(0, 8)}...{addr.substr(50, 58)}</code>
-    </a>
+    </Link>
   )
 }
