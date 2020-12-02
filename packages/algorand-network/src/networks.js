ad2d08196d779d34225777996741826f9d4c3575
Leon <leon390422914@gmail.com>
2020-12-02 16:23:20 +0800

    feat: change bsn network url

--- packages/algorand-network/src/networks.js
+++ packages/algorand-network/src/networks.js
@@ -16,7 +16,7 @@ const networkList = [
     fullName: 'Testnet Network by BSN',
     icon: 'fas fa-vial',
     notification: 'Switched to <b>Testnet</b> provided by BSN.',
-    url: 'https://hk.bsngate.com/api/16fd2bbefb2cb58ff5ca0906e981d1ace6e4fad0e298d1d080c8546baab2ccbd/Algorand-Testnet/algodrest',
+    url: 'https://hk.bsngate.com/api/a831fe50cc554f96b9dc040dd68da36ab8cf13604d2796255411a1211fb776bf/Algorand-Testnet/algodrest',
   }
 ]
 
